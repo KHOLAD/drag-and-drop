ffdcc8d5dcfeb14e65d8f2552431a223dafd7d05
Oleg <olegin64@gmail.com>
2020-09-26 17:01:15 +0300

    base drag and drop handling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const items = [{ name: "John" }, { name: "Smith" }, { name: "Bob" }];
+let prevX = -1;
+
+const dragover = (event) => {
+  event.preventDefault();
+
+  if (prevX === -1) {
+    prevX = event.pageX;
+    return false;
+  }
+
+  const draggedElement = document.querySelector(".dragging");
+  event.currentTarget.parentNode.insertBefore(
+    draggedElement,
+    prevX < event.pageX ? event.currentTarget.nextSibling : event.currentTarget
+  );
+  prevX = event.pageX;
+};
+
+const dragstart = (event) => {
+  event.currentTarget.classList.add("dragging");
+};
+
+const drop = (event) => {
+  console.log("drop");
+};
+
+const dropEnd = (event) => {
+  console.log("end");
+  event.currentTarget.classList.remove("dragging");
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {items.map((item, i) => {
+        return (
+          <div
+            onDragEnd={dropEnd}
+            onDragStart={dragstart}
+            onDragOver={dragover}
+            onDrop={drop}
+            draggable="true"
+            className="box"
+            key={item.name + "-" + i}
+          >
+            {item.name}
+          </div>
+        );
+      })}
     </div>
   );
 }
